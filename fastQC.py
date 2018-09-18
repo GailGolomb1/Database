4de023ff30efc70929a39392b8d388bdf6934bf4
GailGolomb1 <39998595+GailGolomb1@users.noreply.github.com>
2018-09-18 13:37:17 -0400

    latest file
    most recent version of fastQC.py

--- fastQC.py
+++ fastQC.py
@@ -1,53 +1,88 @@
 import sqlite3
-conn = sqlite3.connect('QC_DB.sqlite')
+conn = sqlite3.connect('QCDB.sqlite')
 c = conn.cursor()
 
 file = open('fastqc_data.txt', 'r')
+i = 0
 for line in file.readlines():
     perBase = '#Base	Mean	Median	Lower Quartile   Upper Quartile    10th Percentile    90th Percentile'
     perBase.split("\t")
+    print line
+    i = i + 1
+    if i==20:
+        break
 
-    if line == '>>module Per base sequence quality':
-        c.execute("""INSERT INTO Per base sequence quality ("#Base",    "Mean",    "Median",    "Lower Quartile",   "Upper Quartile",   "10th Percentile",  "90th Percentile") Values (
-        "#Base",    "Mean",    "Median",   "Lower Quartile",    "Upper Quartile",   "10th Percentile",  "90th Percentile");""")
+    if '>>Per base sequence quality' in line:
+        print ("Module 1")
 
-        print c.fetchall(file.readlines())
-    elif line != '>>module Per base sequence quality':
+        c.execute("""INSERT INTO '>>Per base sequence quality   pass'('#Base',    'Mean',    'Median',    'Lower Quartile',   'Upper Quartile',   '10th Percentile',  '90th Percentile') Values(
+        '#Base',    'Mean',    'Median',   'Lower Quartile',    'Upper Quartile',   '10th Percentile',  '90th Percentile');""")
+
+        print c.fetchall()
+    elif line != '>>Per base sequence quality' in line:
         file.close()
-    else:
-        print ("Error")
+    #else:
+    #    print ("Error")
 file.close()
+####################################
+#file = open('fastqc_data.txt', 'r')
+i=0
+for line in file.readlines():
+    perBase = '#Tile	Base	Mean'
+    perBase.split("\t")
+    print line
+    i = i + 1
+    if i==20:
+        break
+
+    if '>>Per tile sequence quality' in line:
+        print ("Module 2")
+
+        c.execute("""INSERT INTO '>>Per base sequence quality    pass'('#Tile',    'Base',	'Mean') Values ('#Tile',  'Base',   'Mean');""")
+        print c.fetchall()
+    elif line != '>>Per tile sequence quality' in line:
+        file.close()
+    #else:
+    #    print("Error")
 
 
-perBase = '#Tile	Base	Mean'
-perBase.split("\t")
-if line == ">>Per tile sequence quality":
-    c.execute("""INSERT INTO Per base sequence quality ("#Tile",    "Base",	"Mean") Values ("#Tile",  "Base",   "Mean");""")
-    print c.fetchall()
-else:
-    print("Error")
+#######################
+#file = open('fastqc_data.txt', 'r')
+for line in file.readlines():
+    perBase = 'Quality  Count'
+    perBase.split("\t")
+    print line
+    i = i + 1
+    if i==20:
+        break
+
+    if line == '>>Per sequence quality scores' in lines:
+        print("Module 3")
+        c.execute("""INSERT INTO 'Per base sequence quality    pass' ('Quality', 'Count') Values('Quality', 'Count');""")
+        print c.fetchall()
+    elif line != '>>Per sequence quality scores' in lines:
+        file.close()
 
 
 
-perBase = 'Quality Count'
-perBase.split("\t")
+    #else:
+    #    print ("Error")
 
-if line == '>>Per sequence quality scores':
-    c.execute("""INSERT INTO Per base sequence quality ("Quality", "Count") Values ("Quality", "Count");""")
-    print c.fetchall()
-else:
-    print ("Error")
+for line in file.readlines():
+    perBase = '#Base	G	A	T	C'
+    perBase.split("\t")
+    print line
+    i = i + 1
+    if i==20:
+        break
 
-
-perBase = '#Base	G	A	T	C'
-perBase.split("\t")
-
-if line == '>>Per base sequence content':
-    c.execute("""INSERT INTO Per base sequence quality ("#Base",	"G",	"A",	"T",	"C") Values (
-    "#Base",	"G",	"A",	"T",	"C");""")
-    print c.fetchall()
-else:
-    print("Error")
+    if line == '>>Per base sequence content' in line:
+        c.execute("""INSERT INTO Per base sequence quality ("#Base",	"G",	"A",	"T",	"C") Values (
+        "#Base",	"G",	"A",	"T",	"C");""")
+        print ("Module 4")
+        print c.fetchall()
+    elif line != '>>Per base sequence content' in line:
+        file.close()
 
 
 perBase = '#GC Content Count'
